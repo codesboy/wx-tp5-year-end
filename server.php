5ed66515494d4d46b52089a55515e1355dc2f1d5
codesboy <565195693@qq.com>
2018-01-18 19:50:13 +0800

    update

---
+++ server.php
@@ -0,0 +1,12 @@
+<?php
+
+// [ 应用入口文件 ]
+namespace think;
+
+define('APP_PATH', __DIR__ . '/application/');
+
+// 加载基础文件
+require __DIR__ . '/thinkphp/base.php';
+
+// 执行应用并响应
+Container::get('app',[APP_PATH])->bind('push/Worker')->run()->send();
