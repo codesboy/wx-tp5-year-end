f6e4713fd54cd2a08ea30911204a127361c6334a
codesboy <565195693@qq.com>
2018-01-23 21:35:46 +0800

    update

--- public/wxmp/pages/dev/dev.js
+++ public/wxmp/pages/dev/dev.js
@@ -1,22 +1,34 @@
-//welcome.js  
-//获取应用实例  
+// pages/dev.js
 var app = getApp();
+var array = [];
 Page({
-    data: {
-        motto: 'Hello World',
-        userInfo: {},
-        tempFilePaths:null,
-        per:'',//上传进度
-        btnDisable:false,//submit按钮的禁用状态
-    },
-    onLoad: function () {
-        
-    },
 
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        psw:'周波',
+        notadmin:false,
+        tempFilePaths: null,
+        per: '',//上传进度
+        btnDisable: false,//submit按钮的禁用状态
+        imgs: []
+    },
+    
+    // 密码校验
+    bindBlur: function (e) {
+        // var _this = this;
+        // console.log(e.detail.value)
+        if (e.detail.value==this.data.psw){
+            this.setData({
+                notadmin:false
+            })
+        }
+    },
 
 
     // 上传图片
-    chooseimg:function(){
+    chooseimg: function () {
         let _this = this;
         wx.chooseImage({
             count: 1, // 默认9
@@ -25,21 +37,21 @@ Page({
             success: function (res) {
                 // console.log(res)
                 var tempFilePaths = res.tempFilePaths;
-                if (tempFilePaths){
+                if (tempFilePaths) {
                     _this.setData({
                         tempFilePaths: res.tempFilePaths
                     });
-                }else{
+                } else {
                     wx.showToast({
                         title: '没有得到tempFilePaths',
                     })
                 }
             },
-            fail:function(e){
+            fail: function (e) {
                 wx.showModal({
                     title: '错误',
                     content: e.errMsg,
-                    showCancel:false
+                    showCancel: false
                 })
             }
         });
@@ -53,39 +65,46 @@ Page({
             urls: this.data.imageList
         });
     },
-    userUpload: function(e){
-        let _this= this;
-        let token = wx.getStorageSync('token');
+    teamUpload: function (e) {
+        let _this = this;
+        // let token = wx.getStorageSync('token');
         // console.log(e.detail);
         // console.log(this.data);
         // console.log(app.globalData)
         const uploadTask = wx.uploadFile({
-            // url: 'http://year.com/api/v1/upload/user', //后台图片上传接口
-            url: app.globalData.baseUrl +'/sign', //后台图片上传签到接口
+            url: app.globalData.baseUrl + '/upteamimg', 
             filePath: this.data.tempFilePaths[0],
             name: 'image',
-            header: {"token":token},
+            // header: { "token": token },
             success: function (res) {
-                var data =JSON.parse(res.data);
+                // console.log(res.data)
+                // return false
+                var data = JSON.parse(res.data);
                 console.log(data);
                 // console.log(data)
-                if (data.avatarurl){
+                if (data.msg == '团队照上传成功') {
+                    // var arr_imgs=_this.data.imgs.push(data.img);
+                    array.push(data.img);
+                    console.log(_this.data.imgs)
                     _this.setData({
-                        btnDisable:true,
-                        tempFilePaths: ''
+                        // btnDisable: true,
+                        tempFilePaths: '',
+                        per:0,
+                        imgs:array
                     });
+                    console.log(_this.data.imgs)
                     // app.globalData.avatarurl=data;
                     // 把用户头像图片文件名存到本地缓存
-                    wx.setStorageSync('avatarurl', data.avatarurl);
+                    // wx.setStorageSync('avatarurl', data.avatarurl);
                     // console.log(app.globalData)
                     wx.showToast({
-                        title: '签到成功!',
-                        duration:3000
+                        title: '团队照上传成功',
+                        duration: 3000
                     });
                     // wx.switchTab({
                     //     url:'/pages/barrage/barrage'
                     // });
-                }else{
+                } else {
                     wx.showModal({
                         title: '错误',
                         content: res.data,
@@ -109,9 +128,18 @@ Page({
             // console.log('已经上传的数据长度', res.totalBytesSent)
             // console.log('预期需要上传的数据总长度', res.totalBytesExpectedToSend)
         })
-        
+
+    },
+
+    // 开启投票功能
+    openVote:function(){
+        wx.setStorageSync('openVote', 1)
+    },
+
+    // 结束投票
+    closeVote:function(){
+        wx.setStorageSync('openVote', 2)
     }
 
 
-    
-})  
+})
--- public/wxmp/pages/vote/vote.js
+++ public/wxmp/pages/vote/vote.js
@@ -1,66 +1,95 @@
 // pages/vote/vote.js
+var app = getApp();
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-  
-  },
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        voteShow: true,
+        tmimgs:null,
+        isVote:false,//是否已经投过票
+    },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
-  },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+        var _this = this;
+        var openVote = wx.getStorageSync('openVote');
+        if(openVote==1){
+            this.setData({
+                voteShow:false
+            })
+        }else{
+            this.setData({
+                voteShow: true
+            })
+        };
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
+        // 获取所有团队图片并渲染
+        wx.request({
+            url: app.globalData.baseUrl +'/gettm',
+            method:'POST',
+            success:function(res){
+                // console.log(res.data)
+                // return false
+                // var tmimgs = JSON.parse(res.data);
+                _this.setData({
+                    tmimgs:res.data
+                });
+                console.log(_this.data)
+            }
+        })
+    },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
+    // 进行投票
+    vote:function(e){
+        var _this = this;
+        // console.log(e)
+        if(this.data.isVote){
+            wx.showToast({
+                title: '您已经投过票了,请勿重复投票!',
+                duration:3000,
+                icon:'none'
+            });
+            return false;
+        };
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
+        wx.request({
+            url: app.globalData.baseUrl+'vote',
+            method:'POST',
+            data:{
+                tm_id: e.currentTarget.dataset.id
+            },
+            success:function(res){
+                console.log(res.data)
+                if (res.data.msg =='投票成功'){
+                    _this.setData({
+                        isVote: true
+                    });
+                    wx.showToast({
+                        title: '投票成功!',
+                        duration: 3000
+                    });
+                }
+                
+            }
+        })
+    },
+
 })
